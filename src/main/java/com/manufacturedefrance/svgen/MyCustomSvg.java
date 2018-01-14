7ab2ff02870cae31af3a64f5754a917388534512
Eliot GODARD <eliot.godard@gmail.com>
2018-01-14 22:30:35 +0100

    removed code smells from MyCustomSvg.java

--- src/main/java/com/manufacturedefrance/svgen/MyCustomSvg.java
+++ src/main/java/com/manufacturedefrance/svgen/MyCustomSvg.java
@@ -25,7 +25,8 @@ import com.manufacturedefrance.techdrawgen.MyCustomSvgEnhanced.ShiftMode;
 import com.manufacturedefrance.utils.MyPath2D;
 
 public class MyCustomSvg extends SvgComponent{
-	private double x, y;
+	private double x;
+	private double y;
 	private Rectangle2D bounds;
 	private ArrayList<SvgComponent> svgTree;
 	private Padding padding;
@@ -39,7 +40,7 @@ public class MyCustomSvg extends SvgComponent{
 		this.padding = new Padding(0);
 		this.x = 0;
 		this.y = 0;
-		this.svgTree = new ArrayList<SvgComponent>();
+		this.svgTree = new ArrayList<>();
 		this.hasBorders = false;
 		this.borderSc = null;
 		this.rememberedSc = this.sc.clone();
@@ -51,7 +52,7 @@ public class MyCustomSvg extends SvgComponent{
 		this.padding = g.padding.clone();
 		this.x = g.x;
 		this.y = g.y;
-		this.svgTree = new ArrayList<SvgComponent>();
+		this.svgTree = new ArrayList<>();
 		for(SvgComponent c : g.svgTree)
 			this.svgTree.add(c.clone());
 		this.hasBorders = g.hasBorders;
@@ -69,12 +70,10 @@ public class MyCustomSvg extends SvgComponent{
 	}
 	
 	public double getWidth() {
-		//return this.bounds.getWidth();
 		return this.bounds.getWidth() + this.padding.getHorizontalPadding();
 	}
 
 	public double getHeight() {
-		//return this.bounds.getHeight();
 		return this.bounds.getHeight() + this.padding.getVerticalPadding();
 	}
 	
@@ -182,22 +181,14 @@ public class MyCustomSvg extends SvgComponent{
 	}
 	
 	public void drawString(String text, double x, double y) {
-		Rectangle2D boundsH = this.rememberedSc.getFont().getStringBounds(text, new FontRenderContext(null, true, true));
-		Rectangle2D boundsV = this.rememberedSc.getFont().createGlyphVector(this.getFontMetrics().getFontRenderContext(), text).getVisualBounds();
-		Rectangle2D bounds = new Rectangle2D.Double(boundsH.getX() + x, boundsV.getY() + y, boundsH.getWidth(), boundsV.getHeight());
+		Rectangle2D strBoundsH = this.rememberedSc.getFont().getStringBounds(text, new FontRenderContext(null, true, true));
+		Rectangle2D strBoundsV = this.rememberedSc.getFont().createGlyphVector(this.getFontMetrics().getFontRenderContext(), text).getVisualBounds();
+		Rectangle2D strBounds = new Rectangle2D.Double(strBoundsH.getX() + x, strBoundsV.getY() + y, strBoundsH.getWidth(), strBoundsV.getHeight());
 		
 		StrSVG str = new StrSVG(text, x, y, this.rememberedSc);
 		svgTree.add(str);
 		
-		this.enlargeBounds(bounds);
-		
-		///// DEBUG
-		/*
-		this.setColor(Color.RED);
-		this.drawRect(bounds);
-		this.setColor(Color.BLACK);
-		*/
-		/////
+		this.enlargeBounds(strBounds);
 	}
 	
 	public void drawSvg(MyCustomSvg svg, double x, double y) {
@@ -209,7 +200,8 @@ public class MyCustomSvg extends SvgComponent{
 	}
 	
 	public void drawSvg(MyCustomSvg svg, double x, double y, ShiftMode hShift, ShiftMode vShift) {
-		double actualX, actualY;
+		double actualX;
+		double actualY;
 		switch(hShift) {
 			case CENTER:
 				actualX = x - svg.getWidth() / 2;
@@ -233,31 +225,29 @@ public class MyCustomSvg extends SvgComponent{
 				break;
 		}
 
-		MyCustomSvg svgClone = (MyCustomSvg) svg.clone();
+		MyCustomSvg svgClone = svg.clone();
 		svgClone.sc = this.rememberedSc.clone();
 		svgTree.add(svgClone);
 		svgClone.setPosition(actualX, actualY);
-		Rectangle2D bounds = new Rectangle2D.Double(actualX, actualY, svgClone.getWidth(), svgClone.getHeight());
-		this.enlargeBounds(bounds);
+		Rectangle2D svgBounds = new Rectangle2D.Double(actualX, actualY, svgClone.getWidth(), svgClone.getHeight());
+		this.enlargeBounds(svgBounds);
 	}
 	
 	public String renderTag() {
-		double curX, curY;
-		curX = - this.bounds.getX() + this.x;
-		curY = - this.bounds.getY() + this.y;
+		double curX = - this.bounds.getX() + this.x;
+		double curY = - this.bounds.getY() + this.y;
 
-		String balisesIntermediaires = "";
+		StringBuilder balisesIntermediaires = new StringBuilder();
 		for(SvgComponent svgComponent : this.svgTree)
-			balisesIntermediaires += svgComponent.renderTag() + "\n";
+			balisesIntermediaires.append(svgComponent.renderTag() + "\n");
 
 		GTag gTagPadding = new GTag();
 		gTagPadding.translate(this.padding.getLeftPadding(), this.padding.getTopPadding());
-		String gPadding = gTagPadding.render(balisesIntermediaires);
+		String gPadding = gTagPadding.render(balisesIntermediaires.toString());
 
 		if(this.hasBorders) {
-			double curW, curH;
-			curW = this.bounds.getWidth() + this.padding.getHorizontalPadding();
-			curH = this.bounds.getHeight() + this.padding.getVerticalPadding();
+			double curW = this.bounds.getWidth() + this.padding.getHorizontalPadding();
+			double curH = this.bounds.getHeight() + this.padding.getVerticalPadding();
 
 			String gBorder = "";
 			gBorder += "<rect ";
@@ -278,13 +268,6 @@ public class MyCustomSvg extends SvgComponent{
 
 		return gTagTransform.render(gPadding);
 	}
-
-	private String tabuler(String input){
-		String output = "";
-		for(String line : input.split("\n"))
-			output += "\t" + line + "\n";
-		return output;
-	}
 	
 	public void clear() {
 		this.svgTree.clear();
@@ -293,13 +276,6 @@ public class MyCustomSvg extends SvgComponent{
 	public void writeToSVG(Path outputFilePath) {
 		double w = this.bounds.getWidth() + this.padding.getHorizontalPadding();
 		double h = this.bounds.getHeight() + this.padding.getVerticalPadding();
-
-		/*
-		String output = "";
-		output += "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + w + "\" height=\"" + h + "\" >\n";
-		output += tabuler(this.renderTag());
-		output += "</svg>";
-		*/
 
 		SvgTag svgTag = new SvgTag();
 		svgTag.width(w);
@@ -316,11 +292,11 @@ public class MyCustomSvg extends SvgComponent{
 	
 	private void enlargeBounds(Shape shape) {
 		Shape transformedShape = this.rememberedSc.getTransform().createTransformedShape(shape);
-		Rectangle2D bounds = transformedShape.getBounds2D();
+		Rectangle2D curBounds = transformedShape.getBounds2D();
 		if(this.bounds == null)
-			this.bounds = bounds;
+			this.bounds = curBounds;
 		else
-			this.bounds.add(bounds);
+			this.bounds.add(curBounds);
 	}
 	
 	public MyCustomSvg clone() {
